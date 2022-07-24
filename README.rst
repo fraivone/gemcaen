91e2feeb094d33f44a0ca012e3be6208d53b4f02
Francesco Ivone <francesco.ivone@cern.ch>
2022-07-24 20:46:16 +0200

    README, setter method

---
+++ README.rst
@@ -0,0 +1,3 @@
+1. Install poetry [link](https://python-poetry.org/docs/#osx--linux--bashonwindows-install-instructions)
+2. Install the project with poetry Install
+3. Make sure that pycanehv finds the library libcaenhvwrapper.so.6.1. You can download it here https://www.caen.it/download/?filter=CAEN%20HV%20Wrapper%20Library. ```/pycaenhv/src/pycaenhv/utils/dll_finder.py``` will look for it!!
--- gemcaen/gemcaen.py
+++ gemcaen/gemcaen.py
@@ -2,6 +2,7 @@ import pathlib
 import yaml
 import functools
 import tableformatter as tf
+import numbers
 
 from pycaenhv.wrappers import init_system, deinit_system, get_board_parameters, get_crate_map, get_channel_parameters,get_channel_parameter, list_commands,get_channel_parameter_property,get_channel_name,set_channel_parameter
 from pycaenhv.enums import CAENHV_SYSTEM_TYPE, LinkType
@@ -15,18 +16,25 @@ def load_config():
         cfg = yaml.full_load(ymlfile)
     return cfg
 
-def singleton(cls):
-    """ allow only one istance of a class """
-    @functools.wraps(cls)
-    def wrapper_singleton(*args, **kwargs):
-        if not wrapper_singleton.istance:
-            wrapper_singleton.istance = cls(*args, **kwargs)
-        return wrapper_singleton.istance
+# def repeat_until(_func=None,*,max_rep=5,success_value=0,sleep_time= 1):
+#     """ Repeat the execution of a function max_rep of times. If success, break """
+#     def decorator_repeat_until(func):
+#         @functools.wraps(func)
+#         def wrapper(*args,**kwargs):
+#             for _ in range(max_rep): 
+#                 print(f"Call {_} to {func.__name__!r}")
+#                 value = func(*args,**kwargs)
+#                 if value == success_value: break
+#                 time.sleep(sleep_time)
+#             return value
+#         return wrapper
+#     if _func is None:
+#         return decorator_repeat_until
+#     else:
+#         return decorator_repeat_until(_func)
+    
+#     return decorator_repeat_until
 
-    wrapper_singleton.istance = None
-    return wrapper_singleton
-
-@singleton
 class BoardBase:
     def __init__(self,setup_name):
         self.setup_name = setup_name
@@ -50,7 +58,6 @@ class BoardBase:
                 return True
         raise ValueError(self.setup_name, " has an invalid/incomplete configuration in ",CONFIG_PATH)
 
-
     def get_cratehandle(self):
         system_type = CAENHV_SYSTEM_TYPE[self.cfg['CAENHV_BOARD_TYPE']]
         link_type = LinkType[self.cfg['CAENHV_LINK_TYPE']]
@@ -64,13 +71,31 @@ class BoardBase:
         except CAENHVError as err:
             print(f"Got error: {err}\nExiting ...")
 
+    def validChannel(self,channel_index:int):
+        if channel_index not in range(self.n_channels):
+            raise ValueError("Invalid channel index ",channel_index)
+            return False
+        return True
+    def validQuantity(self,channel_index:int,quantity:str):
+        if quantity not in self.channel_quantities_map[channel_index]:
+            raise ValueError("Invalid quantity ",quantity,". Valid quantities are ",self.channel_quantities_map[channel_index])
+            return False
+        return True
+
+    def map_channels(self):
+        channel_names_map = dict()
+        channel_quantities_map = dict()
+        for ch in range(self.n_channels):
+            channel_names_map[ch] = get_channel_name(self.handle,self.board_slot,ch)
+            channel_quantities_map[ch] = get_channel_parameters(self.handle,self.board_slot,ch)
+        return  channel_names_map,channel_quantities_map
 
     def print_board_status(self):
         print(f"Board {self.board_name} status --> {self.board_description}")
         cols, rows = ["Ch_Number","Ch_Name"], []
         
-        for ch,channel_name in self.channel_map.items():
-            row = [channel_name]
+        for ch,channel_name in self.channel_names_map.items():
+            row = [ch,channel_name]
             for quantity in self.channel_quantities_map[ch]:
                 channel_value = get_channel_parameter(self.handle,self.board_slot,ch,quantity)
                 row.append(round(channel_value,3))    
@@ -78,29 +103,19 @@ class BoardBase:
         
         cols = cols + self.channel_quantities_map[ch] ## assume all board's channels have same quantities
         print(tf.generate_table(rows, cols, grid_style=tf.AlternatingRowGrid()))
-    
-    def map_channels(self):
-        channel_names_map = dict()
-        channel_quantities_map = dict()
-        for ch in range(self.n_channels):
-            channel_names_map[ch] = get_channel_name(self.handle,self.board_slot,ch)
-            channel_quantities_map[ch] = get_channel_parameters(self.handle,self.slot,ch)
-        return  channel_names_map,channel_quantities_map
-
 
     def get_channel_value(self,channel_index:int,quantity:str):
-        if channel_index not in range(self.n_channels):
-            raise ValueError("Invalid channel index ",channel_index)
-        if quantity not in self.channel_quantities_map[channel_index]:
-            raise ValueError("Invalid quantity ",quantity,". Valid quantities are ",self.channel_quantities_map)
-        return get_channel_parameter(self.handle,self.slot,channel_index,quantity)
+        if validChannel(channel_index) and validChannel(channel_index,quantity):
+            return get_channel_parameter(self.handle,self.board_slot,channel_index,quantity)
+
+    def set_channel_value(self,channel_index:int,quantity:str,value):
+        if validChannel(channel_index) and validChannel(channel_index,quantity) and isinstance(value, numbers.Number):
+            set_channel_parameter(self.handle,self.slot,channel_index,quantity, value)
 
 c = BoardBase("IntegrationStand_Scintillator")
 c.print_board_status()
 print(c.get_channel_value(2,"VMon"))
-
-
-
+print(c.set_channel_value(2,"VMon","ciao"))
 
 
 
